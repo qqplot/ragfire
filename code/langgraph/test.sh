23b953b5a968660cbd37735cddac3ba1ceec219e
bae4147 <sh.bae@snu.ac.kr>
2025-03-24 20:41:02 +0900

    requirements.txt

--- code/langgraph/test.sh
+++ code/langgraph/test.sh
@@ -2,17 +2,19 @@
 
 #SBATCH --job-name=inference_rag
 #SBATCH --nodes=1                    
-#SBATCH --gres=gpu:1                  
-#SBATCH --mem=100GB                     
-#SBATCH --cpus-per-task=8            
+#SBATCH --gres=gpu:1              
+#SBATCH --mem=200GB                     
+#SBATCH --cpus-per-task=16            
 #SBATCH --output=./S-%x.%j.out   
 #SBATCH --time=5:00:00       
-#SBATCH --partition=laal_3090                  
+#SBATCH --partition=laal_a6000                  
 
 export PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True
 
 source ${HOME}/.bashrc
 source ${HOME}/anaconda3/bin/activate
 conda activate rag  
+MODEL_NAME="qwen"
 
-srun python code/langgraph/run_rag_inference.py 
+# srun python code/langgraph/run_rag_inference.py 
+deepspeed --num_gpus 1 run_rag_inference_qwen.py --model_name ${MODEL_NAME}
